db32600bf28edec799b47e2ca07801c7acf6c73b
darkkacool-dotcom <darkkacool@gmail.com>
2026-07-19 14:11:49 -0500

    reverted rapier update

--- Scripts/preload/a.gd
+++ Scripts/preload/a.gd
@@ -1,7 +1,1 @@
 extends Node
-
-
-func SetClickThrough(clickthrough: bool) -> void:
-	GetWindow().MousePassthrough = true;
-	print("passthrough now: ", DisplayServer.window_get_flag(DisplayServer.WINDOW_FLAG_MOUSE_PASSTHROUGH))
-	Engine.max_fps = 60 if clickthrough else 60
